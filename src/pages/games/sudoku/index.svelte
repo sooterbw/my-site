08c3b133e79ee79b0cdce379450f9b2c40f2cdf3
Ben Sooter <sooterbw@gmail.com>
2020-10-24 14:34:17 -0500

    Update sudoku UI and functionality.

--- src/pages/games/sudoku/index.svelte
+++ src/pages/games/sudoku/index.svelte
@@ -3,12 +3,24 @@
     import { createBoard } from './sudoku.js';
 
     const board = createBoard();
+    let editBoard = board.map(x => {
+        if(x===0) {
+            return ''
+        } else {
+            return x
+        }
+    });
 </script>
 
 <div class="flex h-screen w-screen justify-center items-center">
-    <div class="sudoku border border-black">
-        {#each board as square, i}
-            <div class="text-2xl h-full w-full {(i==2 | i==5) ? 'border-r' : 'border-none'}">{square}</div>
+    <div class="sudoku border rounded-md">
+        {#each editBoard as square, i}
+            <div class=" 
+            {((i+9) - i%9)%27 == 0 ? 'border-b': ''} 
+            {((i+1)%3==0) ? 'border-r' : ''}
+            hover:bg-gray-800">
+            <input type="text" class="bg-transparent w-full border-none text-2xl text-center py-2" disabled={editBoard[i] === board[i]} bind:value={editBoard[i]}>
+        </div>
         {/each}
     </div>
 </div>
@@ -17,9 +29,6 @@
     .sudoku {
         display: grid;
         grid-template-columns: repeat(9, 1fr);
-        justify-content: center;
-        justify-items: center;
-        align-items: center;
         width: 500px;
         height: 500px;
     }
